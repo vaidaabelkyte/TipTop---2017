fa3b22d83fb2828cacbc614a5e42c0407c37ed6a
vaidaabelkyte <vaidilutia@gmail.com>
2018-04-11 08:31:26 +0100

    delete selected issue fixed

--- BackEnd/src/main/resources/static/js/scripts.js
+++ BackEnd/src/main/resources/static/js/scripts.js
@@ -3,96 +3,80 @@
  */
 
 $(document).ready(function() {
-	$('.delete-food').on('click', function() {
+	$('.delete-food').on('click', function (){
 		/*<![CDATA[*/
-		var path = /*[[@{/}]]*/'remove';
-		/*]]>*/
+	    var path = /*[[@{/}]]*/'remove';
+	    /*]]>*/
 		
 		var id=$(this).attr('id');
 		
 		bootbox.confirm({
-			message: "Are You sure? The action cant be undone.",
-		buttons: {
-			cancel: {
-				label:'<i class="fa fa-times"></i> Cancel'
+			message: "Are you sure to delete? It cant be undone.",
+			buttons: {
+				cancel: {
+					label:'<i class="fa fa-times"></i> Cancel'
+				},
+				confirm: {
+					label:'<i class="fa fa-check"></i> Confirm'
+				}
 			},
-			confirm: {
-				label:'<i class="fa fa-check"></i> Confirm'
+			callback: function(confirmed) {
+				if(confirmed) {
+					$.post(path, {'id':id}, function(res) {
+						location.reload();
+					});
+				}
 			}
-		},
-		callback: function(confirmed) {
-			if(confirmed) {
-				$.post(path, {'id':id}, function(res) {
-					location.reload();
-				});
-			}
-		}
-			
-			
-			
-		});	
+		});
 	});
-
-	var foodList=[];
-	//$('.checkboxFood').click(function() {
-		//var id=$(this).attr('id');
-		//if(this.checked){
-			//foodIdList.push(id);
-			
-		//} else {
-			//foodIdList.splice(foodIdList.indexOf(id), 1);
-		//}
 	
-	//})
+	
+
 	
 	$('#deleteSelected').click(function() {
 		var idList= $('.checkboxFood');
 		var foodIdList=[];
-		for (var i=0; i < idList.length; i++) {
+		for (var i = 0; i < idList.length; i++) {
 			if(idList[i].checked==true) {
 				foodIdList.push(idList[i]['id'])
 			}
 		}
-	
+		
+		console.log(foodIdList);
+		
 		/*<![CDATA[*/
-		var path = /*[[@{/}]]*/'remove';
-		/*]]>*/
-		
-		bootbox.confirm({
-			message: "Are You sure? The action cant be undone.",
-		buttons: {
-			cancel: {
-				label:'<i class="fa fa-times"></i> Cancel'
+	    var path = /*[[@{/}]]*/'removeList';
+	    /*]]>*/
+	    
+	    bootbox.confirm({
+			message: "Are you sure to remove all? It cant be undone.",
+			buttons: {
+				cancel: {
+					label:'<i class="fa fa-times"></i> Cancel'
+				},
+				confirm: {
+					label:'<i class="fa fa-check"></i> Confirm'
+				}
 			},
-			confirm: {
-				label:'<i class="fa fa-check"></i> Confirm'
+			callback: function(confirmed) {
+				if(confirmed) {
+					$.ajax({
+						type: 'POST',
+						url: path,
+						data: JSON.stringify(foodIdList),
+						contentType: "application/json",
+						success: function(res) {
+							console.log(res); 
+							location.reload()
+							},
+						error: function(res){
+							console.log(res); 
+							location.reload();
+							}
+					});
+				}
 			}
-		},
-		callback: function(confirmed) {
-			if(confirmed) {
-				$.ajax({
-					type: 'POST',
-					url: path,
-					data: JSON.stringify(foodIdList),
-					contentType: "application/json",
-					success: function(res) {
-						console.log(res);
-			
-					location.reload()},
-					error: function(res){
-						console.log(res);
-					
-					location.reload();}
-					}
-				
-				});
-			}
-		}
-			
-			
-			
 		});
-		
 	});
 	
 	$("#selectAllFoods").click(function() {
@@ -102,6 +86,4 @@ $(document).ready(function() {
 			$(".checkboxFood").prop("checked",false);
 		}
 	})
-	
 });
-
